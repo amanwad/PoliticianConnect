1fc842773de5a598b85a0426f29a277f318eaec3
Chris Gu <cgu2022@pingry.org>
2020-08-31 23:13:33 -0400

    Added basic accounts

--- client/main.js
+++ client/main.js
@@ -15,12 +15,21 @@ Meteor.call('alternate',
 
 });
 
-AccountTemplates.addField({
-      _id: "home_address",
-      type: "text",
-      displayName: "Home Address",
-      required: true,
-  });
+Template.body.events({
+  'click .sign-out'(event) {
+      console.log("Pressed Logout!");
+      AccountsTemplates.logout();
+  }
+})
+
+// User accounts documentation: https://github.com/meteor-useraccounts/core/blob/master/Guide.md#form-fields-configuration
+AccountsTemplates.addField({
+  _id: "home_address",
+  type: "text",
+  placeholder: "Home Address",
+  displayName: "Home Address",
+  required: true,
+});
 
 //var x = fetch('https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=416%20Vanderveer%20Road%20Bridgewater%20NJ%2008807&levels=country&roles=legislatorLowerBody&key=AIzaSyDB5e9LPNi72wi_R7wqCw6VFmloaQZ0jmM');
 //console.log(x);
